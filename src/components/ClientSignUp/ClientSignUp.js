2086465f428b79dc42f3a6483f81c6effcd6cedc
Alberto Aragon <alberto@alturasoluciones.com>
2018-05-08 12:18:50 -0500

    Agent Login

---
+++ src/components/ClientSignUp/ClientSignUp.js
@@ -0,0 +1,148 @@
+import React, {Component} from 'react';
+import {
+  KeyboardAvoidingView,
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from 'react-native';
+
+import * as urls from '../../constants/api';
+
+const styles = require('./SignUpStyles');
+
+export default class SignUp extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstname: '',
+      lastname: '',
+      email: '',
+      password: '',
+      password_confirmation: '',
+      errorMessage: ''
+    };
+    this.signUpUser = this.signUpUser.bind(this);
+  }
+
+  signUpUser = () => {
+    this.setState({errorMessage: ''});
+    if (this.state.email === '') {
+      this.setState({errorMessage: "El campo de correo no puede estar vacío"})
+    } else if (this.state.password != this.state.password_confirmation) {
+      this.setState({errorMessage: "Las contraseñas no coinciden"})
+    } else {
+      signup_url = urls.BASE_URL + urls.SIGNUP_URI;
+      fetch(signup_url, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          "customer": {
+            "first_name": this.state.firstname,
+            "last_name": this.state.lastname,
+            "email": this.state.email,
+            "password": this.state.password,
+            "password_confirmation": this.state.password_confirmation
+          }
+        }),
+      })
+        .then((response) => {
+          if (response.status === 200) {
+            this.props.navigation.navigate('ClientDashboard');
+          } else if (response.status === 422) {
+            this.setState({errorMessage: "Ya existe un usuario con ese correo electrónico"});
+          }
+        })
+        .catch((error) => this.setState({errorMessage: error.message}));
+    }
+
+
+  };
+
+  render() {
+    return (
+      <KeyboardAvoidingView
+        behaviour='padding'
+        style={styles.sign_up_container}
+      >
+        <ScrollView
+          contentContainerStyle={styles.sign_up_container}
+          keyboardShouldPersistTaps='never'
+        >
+          <View style={styles.sign_up_form_container}>
+            <Text style={styles.sign_up_banner_text}>
+              REGISTRO
+            </Text>
+            <Text>
+              {this.state.errorMessage}
+            </Text>
+            <TextInput
+              style={styles.sign_up_input}
+              onChangeText={(firstname) => this.setState({firstname})}
+              placeholder="NOMBRE"
+              autoCapitalize="none"
+              underlineColorAndroid="#fff"
+            />
+            <TextInput
+              style={styles.sign_up_input}
+              onChangeText={(lastname) => this.setState({lastname})}
+              placeholder="APELLIDO"
+              autoCapitalize="none"
+              underlineColorAndroid="#fff"
+            />
+            <TextInput
+              style={styles.sign_up_input}
+              onChangeText={(email) => this.setState({email})}
+              placeholder="CORREO ELECTRÓNICO"
+              autoCapitalize="none"
+              keyboardType="email-address"
+              underlineColorAndroid="#fff"
+            />
+            <TextInput
+              style={styles.sign_up_input}
+              onChangeText={(password) => this.setState({password})}
+              placeholder="CONTRASEÑA"
+              autoCapitalize="none"
+              onFocus={() => {
+                this.setState({password: ""});
+              }}
+              secureTextEntry={true}
+              underlineColorAndroid="#fff"
+            />
+            <TextInput
+              style={styles.sign_up_input}
+              onChangeText={(password_confirmation) => this.setState({password_confirmation})}
+              placeholder="CONFIRMACIÓN DE CONTRASEÑA"
+              autoCapitalize="none"
+              onFocus={() => {
+                this.setState({password_confirmation: ""});
+              }}
+              secureTextEntry={true}
+              underlineColorAndroid="#fff"
+            />
+          </View>
+          <View style={styles.sign_up_actions_container}>
+            <TouchableOpacity onPress={() => this.props.navigation.navigate('Home')}>
+              <Text style={styles.login_button}>
+                ATRÁS
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={this.signUpUser}
+              style={styles.sign_up_button}
+            >
+              <Text style={styles.sign_up_text}>
+                REGISTRAR
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    )
+  }
+};
---
+++ src/components/CustomerLogin/CustomerLogin.js
@@ -0,0 +1,90 @@
+import React, {Component} from 'react';
+import {
+   KeyboardAvoidingView,
+   ScrollView,
+   StyleSheet,
+   Text,
+   TextInput,
+   TouchableOpacity,
+   View
+} from 'react-native';
+import {SwitchNavigator} from 'react-navigation';
+
+const styles = require('./LoginStyles');
+
+export default class Login extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         email: '',
+         password: '',
+         errorMessage: ''
+      };
+      this.signInUser = this.signInUser.bind(this);
+   }
+
+   signInUser = () => {
+      // TODO Authenticate against API
+      console.log(this.state);
+   };
+
+   render() {
+      return (
+         <KeyboardAvoidingView
+            style={styles.fullSize}
+            behavior="padding"
+         >
+            <ScrollView
+               contentContainerStyle={styles.login_container}
+               keyboardShouldPersistTaps='never'
+               scrollEnabled={false}
+            >
+               <View style={styles.login_form_container}>
+                  <Text>
+                     {this.state.errorMessage}
+                  </Text>
+                  <TextInput
+                     style={styles.login_input}
+                     onChangeText={(email) => this.setState({email})}
+                     value={this.state.email}
+                     placeholder="CORREO ELECTRÓNICO"
+                     autoCapitalize="none"
+                     keyboardType="email-address"
+                     underlineColorAndroid="#fff"
+                  />
+                  <TextInput
+                     style={styles.login_input}
+                     onChangeText={(password) => this.setState({password})}
+                     value={this.state.password}
+                     placeholder="CONTRASEÑA"
+                     autoCapitalize="none"
+                     onFocus={() => this.setState({password: ""})}
+                     secureTextEntry={true}
+                     underlineColorAndroid="#fff"
+                  />
+               </View>
+               <View style={styles.login_actions_container}>
+                  <TouchableOpacity onPress={() => this.props.navigation.navigate('ClientSignUp')}>
+                     <Text style={styles.sign_up_button}>
+                        ¿NO TIENE UNA CUENTA?
+                     </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                     <Text style={styles.sign_up_button}>
+                        ¿OLVIDÓ SU CONTRASEÑA?
+                     </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                     onPress={this.signInUser}
+                     style={styles.login_button}
+                  >
+                     <Text style={styles.login_button_text}>
+                        INICIAR SESIÓN
+                     </Text>
+                  </TouchableOpacity>
+               </View>
+            </ScrollView>
+         </KeyboardAvoidingView>
+      );
+   }
+}
