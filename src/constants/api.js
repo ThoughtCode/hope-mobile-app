25c55c62ea7774e73635ac2886ad719b1e96077e
RJ <rj.ravi111@gmail.com>
2018-07-22 20:55:13 +0530

    api.js file commit and .gitignore file update

---
+++ src/constants/api.js
@@ -0,0 +1,18 @@
+export const BASE_URL = "https://hopeec-production.herokuapp.com";
+// export const BASE_URL = "http://localhost:3000";
+export const SIGNUP_URI = "/api/v1/customers/signup";
+export const AGENT_SIGNUP_URI = "/api/v1/agents/signup";
+export const CUSTOMER_SIGNIN = "/api/v1/customers/signin";
+export const AGENT_SIGNIN = "/api/v1/agents/signin";
+export const CUSTOMER_SIGNOUT = "/api/v1/customers/signout";
+export const AGENT_SIGNOUT = "/api/v1/agents/signout";
+export const CUSTOMER_RECOVER_PASSWORD = "/api/v1/customers/recover_password";
+export const CUSTOMER_UPDATE_PASSWORD = "/api/v1/customers/app_update_password";
+export const AGENT_RECOVER_PASSWORD = "/api/v1/agents/recover_password";
+export const AGENT_UPDATE_PASSWORD = "/api/v1/agents/app_update_password";
+export const SERVICE_TYPES = "/api/v1/customers/service_types";
+export const NEXT_JOBS = "/api/v1/customers/jobs?status=nextjobs&current_page=1&limit=4";
+export const PAST_JOBS = "/api/v1/customers/jobs?status=history&current_page=1&limit=4";
+export const PROPERTIES = "/api/v1/customers/properties/";
+export const JOBS = "/api/v1/agents/jobs";
+
---
+++ src/util/api.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { Alert, AsyncStorage } from 'react-native';
+import * as APILIST from '../constants/api.js';
+import * as globals from '../util/globals';
+export const API = {
+
+    getJobs: (onResponse, data, isHeaderRequired) => {
+        request(onResponse, {}, 'GET', "JSON", isHeaderRequired, APILIST.BASE_URL + APILIST.JOBS + data, buildHeader());
+    },
+
+    applyJob: (onResponse, data, isHeaderRequired) => {
+        request(onResponse, {}, 'POST', "JSON", isHeaderRequired, APILIST.BASE_URL + APILIST.JOBS + data, buildHeader());
+    },
+
+    canApplyJob: (onResponse, data, isHeaderRequired) => {
+        request(onResponse, {}, 'GET', "JSON", isHeaderRequired, APILIST.BASE_URL + APILIST.JOBS + data, buildHeader());
+    },
+}
+
+export const buildHeader = (headerParams = {}) => {
+    var header = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization' : 'Bearer ' + globals.access_token || ''
+    }
+    Object.assign(header, headerParams);
+    return header;
+    
+}
+
+async function request(onResponse, data, type, returnType, isHeaderRequired, featureURL, secureRequest) {
+    let response = '';
+    let responseJSON;
+    console.log("featureURL >>> " + featureURL);
+    console.log("secureRequest " + JSON.stringify(secureRequest));
+    console.log("data >>> " + JSON.stringify(data));
+    console.log("returnType " + returnType);
+    console.log("isHeaderRequired " + isHeaderRequired);
+    console.log("type " + type);
+
+    try {
+        if (type === 'GET') {
+            if (isHeaderRequired) {
+                console.log("Request Call get with Header");
+                response = await fetch(featureURL, {
+                    method: type,
+                    headers: secureRequest
+                });
+            }
+            else {
+                console.log("Request Call get without header");
+                response = await fetch(featureURL, {
+                    method: type,
+                });
+            }
+        }
+        else {
+            console.log("Request Call post with header");
+            response = await fetch(featureURL, {
+                method: type,
+                headers: secureRequest,
+                body: JSON.stringify(data)
+            });
+        }
+        console.log("response " + JSON.stringify(response));
+        console.log("response status " + response.status);
+        if (returnType === 'TEXT') {
+            responseJSON = await response.text();
+        }
+        else {
+            responseJSON = await response.json();
+        }
+        console.log("responseJSON " + JSON.stringify(responseJSON));
+
+        if (response.status == 200) {
+            console.log("onResponse success ");
+            onResponse.success(responseJSON);
+        } else {
+            console.log("onResponse error");
+            onResponse.error(responseJSON);
+        }
+        if (onResponse.complete) {
+            console.log("onResponse complete");
+            onResponse.complete();
+        }
+    } catch (error) {
+        console.log("onResponse catch error " + error);
+        if (onResponse.complete) {
+            console.log("onResponse catch complete");
+            onResponse.complete();
+        }
+    }
+}
