9dd5765917cacb9780fccf049a595a5eab38435d
Mohd Sameer <mohdsameer1310@gmail.com>
2018-11-13 19:07:11 +0530

    [WIP]-push notification

--- App.js
+++ App.js
@@ -11,6 +11,10 @@ import {YellowBox} from 'react-native';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import rootReducer from './src/reducers';
+import {
+  Notifications,
+} from 'expo';
+import registerForNotifications from './src/services/PushNotification';
 
 const store = createStore(rootReducer);
 store.subscribe(() => console.log('store', store.getState()));
@@ -30,9 +34,26 @@ export default class App extends React.Component {
     this.state = {
       fontLoaded: false,
       isLoading : true,
-      isAgentLogin : false
+      isAgentLogin : false,
+      notification: {},
     };
   }
+
+  componentDidMount() {
+    registerForNotifications();
+
+    // Handle notifications that are received or selected while the app
+    // is open. If the app was closed and then opened by tapping the
+    // notification (rather than just tapping the app icon to open it),
+    // this function will fire on the next tick after the app starts
+    // with the notification data.
+    this._notificationSubscription = Notifications.addListener(this._handleNotification);
+  }
+
+  _handleNotification = (notification) => {
+    console.log(JSON.stringify(notification.data))
+    this.setState({notification: notification});
+  };
 
   async componentWillMount() {
 
@@ -60,6 +81,12 @@ export default class App extends React.Component {
   }
   
   render() {
+    if(this.state.notification) {
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Origin: {this.state.notification.origin}</Text>
+        <Text>Data: {JSON.stringify(this.state.notification.data)}</Text>
+      </View>
+    }
     if (this.state.fontLoaded) {
       if (this.state.isAgentLogin) {
         return (
---
+++ src/services/PushNotification.js
@@ -0,0 +1,30 @@
+import { Permissions, Notifications } from 'expo';
+import {
+  AsyncStorage
+} from 'react-native';
+
+export default registerForNotifications = async () => {
+  const { status: existingStatus } = await Permissions.getAsync(
+    Permissions.NOTIFICATIONS
+  );
+  let finalStatus = existingStatus;
+  // only ask if permissions have not already been determined, because
+  // iOS won't necessarily prompt the user a second time.
+  if (existingStatus !== 'granted') {
+    // Android remote notification permissions are granted during the app
+    // install, so this will only ask on iOS
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    finalStatus = status;
+  }
+
+  // Stop here if the user did not grant permissions
+  if (finalStatus !== 'granted') {
+    alert('Hey! You might want to enable notifications to receive notifications.');
+    return;
+  }
+
+  // Get the token that uniquely identifies this device
+  let token = await Notifications.getExpoPushTokenAsync();
+  console.log(token)
+  await AsyncStorage.setItem('PushNotificationToken', token);
+}
