a513ffba52adbe54b5e3de141151ca2340809aeb
José Castellanos <neuralboot19@gmail.com>
2018-11-22 08:41:53 -0500

    Comment in profile agent

---
+++ src/components/Agent/AgentScreenComment/AgentComment.js
@@ -0,0 +1,165 @@
+import React, {Component} from 'react';
+import {Text, TouchableOpacity, View, ScrollView, Image, Dimensions,SafeAreaView,Alert,TextInput,Keyboard, KeyboardAvoidingView, AsyncStorage} from 'react-native';
+import { StackActions, NavigationActions } from 'react-navigation';
+import StarRating from '../../../lib/react-native-star-rating';
+import EvilIcons from '@expo/vector-icons/EvilIcons';
+const {height , width} = Dimensions.get('window')
+import Ionicons from '@expo/vector-icons/Ionicons'
+import * as globals from '../../../util/globals';
+import { API } from '../../../util/api';
+import AgentJobListScreen from '../AgentJobListScreen/AgentJobListScreen';
+
+const styles = require('./AgentCommentStyles');
+
+const IMAGES = {
+    TOP_BACKGROUND : require("../../../../assets/img/topbg.png")
+}
+
+export default class AgentComment extends Component {
+
+    //======================================================================
+    // constructor
+    //======================================================================
+
+    constructor(props){
+        super(props)
+        this.state = {
+            userData : null,
+            isLoading : true,
+            firstName : globals.first_name,
+            lastName : globals.last_name,
+            avatar : globals.avatar,
+            currentPassword : "",
+            newPassword : "",
+            confirmPassword : "",
+        }
+    }
+
+    //======================================================================
+    // componentDidMount
+    //======================================================================
+
+    componentDidMount(){
+      console.log(this.props)
+      console.log(this.state)
+    }
+
+    //======================================================================
+    // tapJobApplyTap
+    //======================================================================
+
+    updatePasswordTap = () =>{
+
+        if(this.state.currentPassword == globals.password){
+            if(this.state.newPassword == this.state.confirmPassword){
+                data = {
+                    "agent": {
+                        "current_password": this.state.currentPassword,
+                        "password": this.state.newPassword,
+                        "password_confirmation": this.state.confirmPassword
+                    }
+                }
+                API.updatePassword(this.updatePasswordResponse,data,true);
+            }else{
+                Alert.alert(globals.APP_NAME,"Mismatch new password")
+            }
+        }else{
+            Alert.alert(globals.APP_NAME,"Current password wrong")
+        }
+    }
+
+    //======================================================================
+    // jobApplyResponse
+    //======================================================================
+
+    updatePasswordResponse = {
+        success: (response) => {
+            try {
+                console.log("jobApplyResponse data-->"+JSON.stringify(response))
+                Alert.alert("Noc Noc",response.message,[{text: 'OK', onPress: () => {
+                    AsyncStorage.clear().then(()=>{
+                        this.props.navigation.navigate("AgentLogin")
+                    })
+                }}])
+            } catch (error) {
+                console.log('jobApplyResponse catch error ' + JSON.stringify(error));
+            }
+        },
+        error: (err) => {
+            console.log('jobApplyResponse error ' + JSON.stringify(err));
+        },
+        complete: () => {
+        }
+    }
+
+    //======================================================================
+    // setFocus
+    //======================================================================
+
+    setFocus = (textField) =>{
+        this[textField].focus()
+    }
+
+    //======================================================================
+    // render
+    //======================================================================
+
+    render(){
+        var initials = globals.first_name.charAt(0) || ""
+        initials += globals.last_name.charAt(0) || ""
+        return(
+            <SafeAreaView style={styles.container}>
+                 <KeyboardAvoidingView
+                    style={{flex:1}}
+                    behavior="padding">
+                        <ScrollView style={{flex:1}} bounces={false}>
+                        <View>
+                            <Ionicons name={"ios-arrow-back"} size={40} style={styles.backButtonImage} onPress={() => this.props.navigation.goBack()} />
+                            <Image source={IMAGES.TOP_BACKGROUND} style={styles.topImage} resizeMode={"cover"} resizeMethod={"auto"}/>
+                                <View style={styles.profileView}>
+
+                                    {(this.state.avatar != "")?
+                                        <Image source={{uri : this.state.avatar+'?time=' + new Date()}} style={styles.profileImage} resizeMode={"cover"} defaultSource={require("../../../../assets/img/profile_placehoder.png")}/>
+                                        :
+                                        <View style={[styles.profileImage, { backgroundColor: 'gray', alignItems: 'center', justifyContent: 'center' }]} >
+                                            <Text style={{ color: '#fff' }}>{initials}</Text>
+                                        </View>
+                                    }
+                                </View>
+
+                                <View style={{alignItems:'center',justifyContent:'center'}}>
+                                    <Text style={{fontSize:20,fontWeight:'600'}}>{this.state.firstName + " "+ this.state.lastName}</Text>
+                                </View>
+
+
+                            <View style={styles.topTitleView}>
+                                <Text style={styles.mainTitleText}>{"Comentarios"}</Text>
+                            </View>
+                        </View>
+
+                        <View style={{flex:1,marginHorizontal:20}}>
+                            <View style={styles.textInputVieW}>
+                                <Text style={styles.textInputTitleText}>{"José Castellanos"}</Text>
+                                <StarRating
+                                    disabled={true}
+                                    emptyStar={'ios-star-outline'}
+                                    fullStar={'ios-star'}
+                                    halfStar={'ios-star-half'}
+                                    iconSet={"Ionicons"}
+                                    maxStars={5}
+                                    rating={"2"}
+                                    starSize={20}
+                                    fullStarColor={'#ffd700'}
+                                />
+                            </View>
+                            <View>
+                              <Text style={styles.textInputCommentText}>{"Comentario texto"}</Text>
+                            </View>
+                        </View>
+
+                    </ScrollView>
+                </KeyboardAvoidingView>
+            </SafeAreaView>
+        )
+    }
+}
