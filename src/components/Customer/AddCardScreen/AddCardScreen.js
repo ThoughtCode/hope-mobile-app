0d544ff11f74f33a51888203523ae7002c6e9143
Jose Castellanos <neuralboot19@gmail.com>
2019-01-18 09:40:08 -0500

    Payment id

--- src/components/Customer/AddCardScreen/AddCardScreen.js
+++ src/components/Customer/AddCardScreen/AddCardScreen.js
@@ -20,6 +20,7 @@ export default class AddCardScreen extends React.Component {
   render() {
     let yourAlert = `document.getElementById("email").value += '${this.state.email}';
       document.getElementById("user_id").value += '` + this.state.id +`';`
+      console.log("-----------------> ID ID ID ID ID",this.state.id)
     return (
       <View style={{flex: 1}}>
         <View>
