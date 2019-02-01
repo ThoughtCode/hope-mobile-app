767cb6c02be261d7c150c90da528b41228bcd9a5
Jose Castellanos <neuralboot19@gmail.com>
2019-02-01 16:33:24 -0500

    Add body paument

--- src/components/Customer/AddCardScreen/AddCardScreen.js
+++ src/components/Customer/AddCardScreen/AddCardScreen.js
@@ -37,14 +37,63 @@ export default class AddCardScreen extends React.Component {
           injectedJavaScript={yourAlert}
           source={{ html: `
           <!DOCTYPE html>
-          <html>
-          <head>  
-            
-          </head>
-          <body>
-              <h1> Hola </h1>
-           </body>
-        </html> ` 
+            <html>
+            <head>  
+              <title>Example Paymentez Add Card | PaymentezJs</title>
+              <meta charset="UTF-8">  
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+              <link href="https://cdn.paymentez.com/js/1.0.1/paymentez.min.css" rel="stylesheet" type="text/css" />
+              <script src="https://cdn.paymentez.com/js/1.0.1/paymentez.min.js"></script>
+              <style>
+                .panel {
+                  margin: 0 auto;
+                  padding: 20px;
+                  display: block;
+                  width: 80%;
+                  border-radius: 6px;
+                }
+                .btn {
+                  background: rgb(140,197,65); /* Old browsers */
+                  background: -moz-linear-gradient(top, rgba(140,197,65,1) 0%, rgba(20,167,81,1) 100%); /* FF3.6-15 */
+                  background: -webkit-linear-gradient(top, rgba(140,197,65,1) 0%,rgba(20,167,81,1) 100%); /* Chrome10-25,Safari5.1-6 */
+                  background: linear-gradient(to bottom, rgba(140,197,65,1) 0%,rgba(20,167,81,1) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+                  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#44afe7', endColorstr='#3198df',GradientType=0 );
+                  color: #fff;
+                  display: block;
+                  width: 100%;
+                  border: 1px solid rgba(46, 86, 153, 0.0980392);
+                  border-bottom-color: rgba(46, 86, 153, 0.4);
+                  border-top: 0;
+                  border-radius: 4px;
+                  font-size: 17px;
+                  text-shadow: rgba(46, 86, 153, 0.298039) 0px -1px 0px;
+                  line-height: 34px;
+                  -webkit-font-smoothing: antialiased;
+                  font-weight: bold;
+                  display : block;
+                  margin-top: 20px;
+                }
+                .btn:hover {
+                  cursor: pointer;
+                }
+              </style>
+            </head>
+            <body>
+              <div class="panel">
+                <div class="paymentez-form" id="my-card" data-capture-name="true">
+                  <input class="card-number" name="card-number" placeholder="Card number">
+                  <input class="name" id="the-card-name-id" placeholder="Card Holders Name">
+                  <input class="expiry-month" name="expiry-month">
+                  <input class="expiry-year" name="expiry-year">
+                  <input class="cvc" name="cvc">
+                </div>
+                <button class="btn submit-payment">Save</button>
+                <div id="messages"></div>
+              </div>
+            </body>
+          </html>
+          ` 
           }}
         />
         <View style={{ alignItems: 'center', justifyContent: 'center', marginVertical: 10 }}>
